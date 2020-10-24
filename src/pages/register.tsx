2da60ac581c675379367f522c2ad81afc3e2724c
mokiong <mokiong1427@gmail.com>
2020-10-25 01:15:56 +0800

    updated

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -26,46 +26,48 @@ const Register: React.FC<registerProps> = ({}) => {
             <Heading as="h1" size="2xl" w="100%">
               Chat-Up
             </Heading>
-            <Formik
-              initialValues={{ username: '', email: '', password: '' }}
-              onSubmit={(values) => console.log(values)}
-            >
-              {({ isSubmitting }) => (
-                <Form>
-                  <InputField
-                    name="username"
-                    placeholder="username"
-                    label="Username"
-                  />
-                  <Box mt={4}>
+            <Box mt={4} paddingLeft="50px" paddingRight="50px">
+              <Formik
+                initialValues={{ username: '', email: '', password: '' }}
+                onSubmit={(values) => console.log(values)}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
                     <InputField
-                      name="email"
-                      placeholder="email"
-                      label="Email"
+                      name="username"
+                      placeholder="username"
+                      label="Username"
                     />
-                  </Box>
-                  <Box mt={4}>
-                    <InputField
-                      name="password"
-                      placeholder="password"
-                      label="Password"
-                      type="password"
-                    />
-                  </Box>
-                  <Center>
-                    <Button
-                      mt={4}
-                      isLoading={isSubmitting}
-                      type="submit"
-                      background="custom.primary"
-                      color="custom.bg"
-                    >
-                      register
-                    </Button>
-                  </Center>
-                </Form>
-              )}
-            </Formik>
+                    <Box mt={4}>
+                      <InputField
+                        name="email"
+                        placeholder="email"
+                        label="Email"
+                      />
+                    </Box>
+                    <Box mt={4}>
+                      <InputField
+                        name="password"
+                        placeholder="password"
+                        label="Password"
+                        type="password"
+                      />
+                    </Box>
+                    <Center>
+                      <Button
+                        mt={4}
+                        isLoading={isSubmitting}
+                        type="submit"
+                        background="custom.primary"
+                        color="custom.bg"
+                      >
+                        register
+                      </Button>
+                    </Center>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
           </Flex>
         </Center>
       </Wrapper>
